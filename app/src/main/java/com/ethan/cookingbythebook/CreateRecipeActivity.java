2e42041a4fd69f4105353b9b395bdf8b5861cf89
Ethan Budreau <ethanbudreau@gmail.com>
2016-07-04 16:47:30 -0500

    Add rudimentary add editText

--- app/src/main/java/com/ethan/cookingbythebook/CreateRecipeActivity.java
+++ app/src/main/java/com/ethan/cookingbythebook/CreateRecipeActivity.java
@@ -2,6 +2,7 @@ package com.ethan.cookingbythebook;
 
 
 import android.app.ActionBar;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,11 +16,20 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class CreateRecipeActivity extends AppCompatActivity {
 
@@ -29,6 +39,11 @@ public class CreateRecipeActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ScreenSlidePagerAdapter viewPagerAdapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -44,6 +59,7 @@ public class CreateRecipeActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         viewPager = (ViewPager) findViewById(R.id.pager);
 
+
         /*
         Creating Adapter and setting that adapter to the viewPager
         setSupportActionBar method takes the toolbar and sets it as
@@ -52,6 +68,7 @@ public class CreateRecipeActivity extends AppCompatActivity {
          */
         viewPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
+
 
         /*
         TabLayout.newTab() method creates a tab view, Now a Tab view is not the view
@@ -99,7 +116,28 @@ public class CreateRecipeActivity extends AppCompatActivity {
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
+        tabLayout.setOnTabSelectedListener(
+                new TabLayout.OnTabSelectedListener() {
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        viewPager.setCurrentItem(tab.getPosition());
+                    }
 
+                    @Override
+                    public void onTabUnselected(TabLayout.Tab tab) {
+
+                    }
+
+                    @Override
+                    public void onTabReselected(TabLayout.Tab tab) {
+
+                    }
+                }
+        );
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -125,105 +163,135 @@ public class CreateRecipeActivity extends AppCompatActivity {
     }
 
 
-      /**
-         * The number of pages (wizard steps) to show in this demo.
-         */
-        private static final int NUM_PAGES = 3;
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 3;
 
-        /**
-         * The pager widget, which handles animation and allows swiping horizontally to access previous
-         * and next wizard steps.
-         */
-        private ViewPager mPager;
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
 
-        /**
-         * The pager adapter, which provides the pages to the view pager widget.
-         */
-        private PagerAdapter mPagerAdapter;
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
 
-    /*
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_create_recipe);
+    @Override
+    public void onStart() {
+        super.onStart();
 
-            // Instantiate a ViewPager and a PagerAdapter.
-            mPager = (ViewPager) findViewById(R.id.pager);
-            mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-            mPager.setAdapter(mPagerAdapter);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "CreateRecipe Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ethan.cookingbythebook/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "CreateRecipe Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ethan.cookingbythebook/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
         @Override
-        public void onBackPressed() {
-            if (mPager.getCurrentItem() == 0) {
-                // If the user is currently looking at the first step, allow the system to handle the
-                // Back button. This calls finish() on this activity and pops the back stack.
-                super.onBackPressed();
-            } else {
-                // Otherwise, select the previous step.
-                mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        public Fragment getItem(int position) {
+
+            String fragmentName = null;
+
+            if (position == 0) {
+                fragmentName = "addRecipeInfo";
             }
-        }
-*/
-        /**
-         * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
-         * sequence.
-         */
-        private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-            public ScreenSlidePagerAdapter(FragmentManager fm) {
-                super(fm);
+            if (position == 1) {
+                fragmentName = "addIngredients";
+            }
+            if (position == 2) {
+                fragmentName = "addSteps";
             }
 
-            @Override
-            public Fragment getItem(int position) {
-
-                String fragmentName=null;
-
-                if (position==0) {
-                    fragmentName="addRecipeInfo";
-                }
-                if (position==1){
-                    fragmentName="addIngredients";
-                }
-                if (position==2){
-                    fragmentName="addSteps";
-                }
-
-                return new ScreenSlidePageFragment(fragmentName);
-            }
-
-            @Override
-            public int getCount() {
-                return NUM_PAGES;
-            }
+            return new ScreenSlidePageFragment(fragmentName);
         }
 
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
 
 
-        public class ScreenSlidePageFragment extends Fragment {
+    public class ScreenSlidePageFragment extends Fragment {
 
-            private String fragmentType;
+        private String fragmentType;
 
-            public ScreenSlidePageFragment(String fragmentName){
-                fragmentType = fragmentName;
-            }
+        public ScreenSlidePageFragment(String fragmentName) {
+            fragmentType = fragmentName;
+        }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-            ViewGroup rootView=null;
+            ViewGroup rootView = null;
 
-            if(fragmentType=="addRecipeInfo"){
+            if (fragmentType.equals("addRecipeInfo")) {
                 rootView = (ViewGroup) inflater.inflate(
                         R.layout.content_add_recipe_info, container, false);
             }
 
-            if(fragmentType=="addIngredients") {
+            if (fragmentType.equals("addIngredients")) {
                 rootView = (ViewGroup) inflater.inflate(
                         R.layout.content_add_ingredients, container, false);
+                FloatingActionButton ingredientAdd = (FloatingActionButton) rootView.findViewById(R.id.ingredientAdd);
+                final ViewGroup finalRootView = rootView;
+                ingredientAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        FrameLayout ll = (FrameLayout) finalRootView.findViewById(R.id.ingredientAddContent);
+
+                        EditText editText = new EditText(getActivity());
+
+                        editText.setText("Hello");
+                        ll.addView(editText);
+                    }
+                });
+
+
             }
 
-            if(fragmentType=="addSteps") {
+            if (fragmentType.equals("addSteps")) {
                 rootView = (ViewGroup) inflater.inflate(
                         R.layout.content_add_steps, container, false);
             }
@@ -231,5 +299,6 @@ public class CreateRecipeActivity extends AppCompatActivity {
             return rootView;
         }
     }
+
 }
 
